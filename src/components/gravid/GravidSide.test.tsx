6b6344c600cfbad4bc7a0dcb58099cd032dec4df
Morten Laukvik <morten.laukvik@nav.no>
2020-11-03 10:41:57 +0100

    Gravid: Sonar fikser

--- src/components/gravid/GravidSide.test.tsx
+++ src/components/gravid/GravidSide.test.tsx
@@ -1,98 +1,95 @@
 import React from 'react'
 import GravidSide from './GravidSide';
-import { render, unmountComponentAtNode } from 'react-dom';
+import {render, unmountComponentAtNode} from 'react-dom';
 import GravidStatus from './GravidStatus';
-import {axe, toHaveNoViolations} from 'jest-axe';
-import { cleanup } from '@testing-library/react';
-import { render as renderTestingLibrary } from '@testing-library/react';
+import {axe} from 'jest-axe';
+import {cleanup, render as renderTestingLibrary} from '@testing-library/react';
 
 describe('GravidSide', () => {
 
-  // expect.extend(toHaveNoViolations)
-
-  let container:Element = document.createElement('div');
+  let htmlDivElement: Element = document.createElement('div');
 
   beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
+    htmlDivElement = document.createElement('div');
+    document.body.appendChild(htmlDivElement);
   });
 
   afterEach(() => {
-    unmountComponentAtNode(container);
-    container.remove();
-    container = document.createElement('div');
+    unmountComponentAtNode(htmlDivElement);
+    htmlDivElement.remove();
+    htmlDivElement = document.createElement('div');
   });
 
-  const INFORMASJON   = 'Informasjon om den ansatte';
-  const FODSELSNR     = 'Fødselsnummer';
-  const TERMINDATO    = 'Termindato';
+  const INFORMASJON = 'Informasjon om den ansatte';
+  const FODSELSNR = 'Fødselsnummer';
+  const TERMINDATO = 'Termindato';
   const TILRETTELEGGE = 'Har dere forsøkt å tilrettelegge arbeidsdagen';
-  const TILTAK        = 'Hvilke tiltak er forsøkt';
-  const VIDERE        = 'Forsøksvis tilrettelegging er i utgangspunktet ';
-  const OMPLASSERING  = 'Er omplassering av den ansatte forsøkt';
+  const TILTAK = 'Hvilke tiltak er forsøkt';
+  const VIDERE = 'Forsøksvis tilrettelegging er i utgangspunktet ';
+  const OMPLASSERING = 'Er omplassering av den ansatte forsøkt';
   const DOKUMENTASJON = 'Dokumentasjon om svagerskapsrelatert';
-  const BEKREFT       = 'Jeg er kjent med at hvis opplysningene jeg har gitt ikke';
-  const SEND_KNAPP    = 'Send søknad';
-  const STATUS_DEFAULT= 'Søknad om unntak fra arbeidsgiveransvar';
+  const BEKREFT = 'Jeg er kjent med at hvis opplysningene jeg har gitt ikke';
+  const SEND_KNAPP = 'Send søknad';
+  const STATUS_DEFAULT = 'Søknad om unntak fra arbeidsgiveransvar';
   const STATUS_VENTER = 'Vennligst vent';
-  const STATUS_OK     = 'Søknaden er mottatt';
-  const STATUS_ERROR  = 'Det oppstod en feil';
+  const STATUS_OK = 'Søknaden er mottatt';
+  const STATUS_ERROR = 'Det oppstod en feil';
 
   it('skal vise kun søknadsskjema som default', () => {
-    render(<GravidSide status={GravidStatus.DEFAULT}/>, container);
-    expect(container.textContent).toContain(STATUS_DEFAULT);
-    expect(container.textContent).not.toContain(STATUS_VENTER);
-    expect(container.textContent).not.toContain(STATUS_OK);
-    expect(container.textContent).not.toContain(STATUS_ERROR);
+    render(<GravidSide status={GravidStatus.DEFAULT}/>, htmlDivElement);
+    expect(htmlDivElement.textContent).toContain(STATUS_DEFAULT);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_VENTER);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_OK);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_ERROR);
   })
 
   it('skal vise venteside når man venter på svar', () => {
-    render(<GravidSide status={GravidStatus.IN_PROGRESS}/>, container);
-    expect(container.textContent).not.toContain(STATUS_DEFAULT);
-    expect(container.textContent).toContain(STATUS_VENTER);
-    expect(container.textContent).not.toContain(STATUS_OK);
-    expect(container.textContent).not.toContain(STATUS_ERROR);
+    render(<GravidSide status={GravidStatus.IN_PROGRESS}/>, htmlDivElement);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_DEFAULT);
+    expect(htmlDivElement.textContent).toContain(STATUS_VENTER);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_OK);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_ERROR);
   })
 
   it('skal vise kvittering når positivt svar', () => {
-    render(<GravidSide status={GravidStatus.SUCCESS}/>, container);
-    expect(container.textContent).not.toContain(STATUS_DEFAULT);
-    expect(container.textContent).not.toContain(STATUS_VENTER);
-    expect(container.textContent).toContain(STATUS_OK);
-    expect(container.textContent).not.toContain(STATUS_ERROR);
+    render(<GravidSide status={GravidStatus.SUCCESS}/>, htmlDivElement);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_DEFAULT);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_VENTER);
+    expect(htmlDivElement.textContent).toContain(STATUS_OK);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_ERROR);
   })
 
   it('skal vise feilmelding når svar feilet', () => {
-    render(<GravidSide status={GravidStatus.ERROR}/>, container);
-    expect(container.textContent).not.toContain(STATUS_DEFAULT);
-    expect(container.textContent).not.toContain(STATUS_VENTER);
-    expect(container.textContent).not.toContain(STATUS_OK);
-    expect(container.textContent).toContain(STATUS_ERROR);
+    render(<GravidSide status={GravidStatus.ERROR}/>, htmlDivElement);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_DEFAULT);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_VENTER);
+    expect(htmlDivElement.textContent).not.toContain(STATUS_OK);
+    expect(htmlDivElement.textContent).toContain(STATUS_ERROR);
   })
 
   it('skal vise samtlige feilmelding når alle felter mangler', () => {
-    render(<GravidSide tilrettelegge={true} validated={true} bekreftet={true} />, container);
-    expect(container.textContent).toContain('Fyll ut gyldig fødselsnummer');
-    expect(container.textContent).toContain('Termindato må fylles ut');
-    expect(container.textContent).toContain('Tiltak må fylles ut');
+    render(<GravidSide tilrettelegge={true} validated={true} bekreftet={true}/>, htmlDivElement);
+    expect(htmlDivElement.textContent).toContain('Fyll ut gyldig fødselsnummer');
+    expect(htmlDivElement.textContent).toContain('Termindato må fylles ut');
+    expect(htmlDivElement.textContent).toContain('Tiltak må fylles ut');
     // expect(container.textContent).toContain('Spesifiser hvilke tiltak som er forsøkt');
-    expect(container.textContent).toContain('Velg omplassering');
-    expect(container.textContent).toContain('Last opp dokumentasjon');
+    expect(htmlDivElement.textContent).toContain('Velg omplassering');
+    expect(htmlDivElement.textContent).toContain('Last opp dokumentasjon');
     // expect(container.textContent).toContain('Bekreft at opplysningene er korrekt');
   })
 
   it('skal vise fødselsnr, termindato og tilrettelegge - som default', () => {
-    render(<GravidSide />, container);
-    expect(container.textContent).toContain(INFORMASJON);
-    expect(container.textContent).toContain(FODSELSNR);
-    expect(container.textContent).toContain(TERMINDATO);
-    expect(container.textContent).toContain(TILRETTELEGGE);
-    expect(container.textContent).not.toContain(TILTAK);
-    expect(container.textContent).not.toContain(VIDERE);
-    expect(container.textContent).not.toContain(OMPLASSERING);
-    expect(container.textContent).not.toContain(DOKUMENTASJON);
-    expect(container.textContent).not.toContain(BEKREFT);
-    expect(container.textContent).not.toContain(SEND_KNAPP);
+    render(<GravidSide/>, htmlDivElement);
+    expect(htmlDivElement.textContent).toContain(INFORMASJON);
+    expect(htmlDivElement.textContent).toContain(FODSELSNR);
+    expect(htmlDivElement.textContent).toContain(TERMINDATO);
+    expect(htmlDivElement.textContent).toContain(TILRETTELEGGE);
+    expect(htmlDivElement.textContent).not.toContain(TILTAK);
+    expect(htmlDivElement.textContent).not.toContain(VIDERE);
+    expect(htmlDivElement.textContent).not.toContain(OMPLASSERING);
+    expect(htmlDivElement.textContent).not.toContain(DOKUMENTASJON);
+    expect(htmlDivElement.textContent).not.toContain(BEKREFT);
+    expect(htmlDivElement.textContent).not.toContain(SEND_KNAPP);
   })
 
 
@@ -117,54 +114,54 @@ describe('GravidSide', () => {
     render(
       <GravidSide
         fnr="123"
-        dato={new Date(2020,9,28)}
+        dato={new Date(2020, 9, 28)}
         tilrettelegge={undefined}
       />
-      , container
+      , htmlDivElement
     );
-    expect(container.textContent).not.toContain(TILTAK);
-    expect(container.textContent).not.toContain(OMPLASSERING);
-    expect(container.textContent).not.toContain(DOKUMENTASJON);
-    expect(container.textContent).not.toContain(BEKREFT);
-    expect(container.textContent).not.toContain(SEND_KNAPP);
+    expect(htmlDivElement.textContent).not.toContain(TILTAK);
+    expect(htmlDivElement.textContent).not.toContain(OMPLASSERING);
+    expect(htmlDivElement.textContent).not.toContain(DOKUMENTASJON);
+    expect(htmlDivElement.textContent).not.toContain(BEKREFT);
+    expect(htmlDivElement.textContent).not.toContain(SEND_KNAPP);
   })
 
   it('skal ikke vise dokumentasjon, bekreft og knapp - før man velger å gå videre', () => {
     render(
       <GravidSide
         fnr="123"
-        dato={new Date(2020,9,28)}
+        dato={new Date(2020, 9, 28)}
         tilrettelegge={false}
         videre={false}
       />
-      , container
+      , htmlDivElement
     );
-    expect(container.textContent).not.toContain(TILTAK);
-    expect(container.textContent).not.toContain(DOKUMENTASJON);
-    expect(container.textContent).not.toContain(BEKREFT);
-    expect(container.textContent).not.toContain(SEND_KNAPP);
+    expect(htmlDivElement.textContent).not.toContain(TILTAK);
+    expect(htmlDivElement.textContent).not.toContain(DOKUMENTASJON);
+    expect(htmlDivElement.textContent).not.toContain(BEKREFT);
+    expect(htmlDivElement.textContent).not.toContain(SEND_KNAPP);
   })
 
   it('skal vise dokumentasjon, bekreft og knapp - etter man har valgt å gå videre', () => {
     render(
       <GravidSide
         fnr="123"
-        dato={new Date(2020,9,28)}
+        dato={new Date(2020, 9, 28)}
         tilrettelegge={false}
         videre={true}
       />
-      , container
+      , htmlDivElement
     );
-    expect(container.textContent).not.toContain(OMPLASSERING);
-    expect(container.textContent).toContain(DOKUMENTASJON);
-    expect(container.textContent).toContain(BEKREFT);
-    expect(container.textContent).toContain(SEND_KNAPP);
+    expect(htmlDivElement.textContent).not.toContain(OMPLASSERING);
+    expect(htmlDivElement.textContent).toContain(DOKUMENTASJON);
+    expect(htmlDivElement.textContent).toContain(BEKREFT);
+    expect(htmlDivElement.textContent).toContain(SEND_KNAPP);
   })
 
   it('should have no a11y violations', async () => {
     const { container } = renderTestingLibrary(<GravidSide
       fnr="123"
-      dato={new Date(2020,9,28)}
+      dato={new Date(2020, 9, 28)}
       tilrettelegge={false}
       videre={true}
     />)
--- src/components/gravid/GravidSide.tsx
+++ src/components/gravid/GravidSide.tsx
@@ -2,14 +2,7 @@ import React, { useState } from 'react';
 import { Column, Row } from 'nav-frontend-grid';
 import Panel from 'nav-frontend-paneler';
 import { Feilmelding, Ingress, Normaltekst } from 'nav-frontend-typografi';
-import {
-  BekreftCheckboksPanel,
-  Feiloppsummering,
-  Radio,
-  RadioGruppe,
-  SkjemaGruppe,
-  Textarea
-} from 'nav-frontend-skjema';
+import { BekreftCheckboksPanel, Feiloppsummering, Radio, RadioGruppe, SkjemaGruppe, Textarea } from 'nav-frontend-skjema';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import { FeiloppsummeringFeil } from 'nav-frontend-skjema/src/feiloppsummering';
 import Skillelinje from '../Skillelinje';
@@ -33,21 +26,21 @@ const EMPTY = '';
 const GravidSide = (props: GravidSideProps) => {
   const [status, setStatus] = useState<number>(props.status || GravidStatus.DEFAULT);
   const [validated, setValidated] = useState<boolean>(false);
-  const [dato, setDato] = useState<Date|undefined>(props.dato || undefined);
+  const [dato, setDato] = useState<Date | undefined>(props.dato || undefined);
   const [datoFeilmelding, setDatoFeilmelding] = useState<string>(!validated ? EMPTY : REQUIRED_INPUT);
   const [fnr, setFnr] = useState<string>(props.fnr || EMPTY);
   const [fnrValid, setFnrValid] = useState<boolean>(isValidFnr(fnr));
-  const [fnrFeilmelding, setFnrFeilmelding] = useState<string>(!validated ? EMPTY :(validated && fnrValid) ? EMPTY : INVALID_FNR );
-  const [tilrettelegge, setTilrettelegge] = useState<boolean|undefined>(props.tilrettelegge);
-  const [tilretteleggeFeilmelding, setTilretteleggeFeilmelding] = useState<string>(!validated ? EMPTY : REQUIRED_INPUT);
+  const [fnrFeilmelding, setFnrFeilmelding] = useState<string>(!validated ? EMPTY : (validated && fnrValid) ? EMPTY : INVALID_FNR);
+  const [tilrettelegge, setTilrettelegge] = useState<boolean | undefined>(props.tilrettelegge);
+  const [tilretteleggeFeilmelding] = useState<string>(!validated ? EMPTY : REQUIRED_INPUT);
   const [tiltak, setTiltak] = useState<string>(props.tiltak || '');
   const [tiltakFeilmelding, setTiltakFeilmelding] = useState<string>(!validated ? EMPTY : REQUIRED_INPUT);
   const [tiltakBeskrivelse, setTiltakBeskrivelse] = useState<string>(props.tiltakBeskrivelse || EMPTY);
   const [tiltakBeskrivelseFeilmelding, setTiltakBeskrivelseFeilmelding] = useState<string>(!validated ? EMPTY : REQUIRED_INPUT);
   const [omplassering, setOmplassering] = useState<string>(props.omplassering || EMPTY);
   const [omplasseringFeilmelding, setOmplasseringFeilmelding] = useState<string>(!validated ? EMPTY : REQUIRED_INPUT);
-  const [dokumentasjon, setDokumentasjon] = useState<File>( );
-  const [dokumentasjonFeilmelding, setDokumentasjonFeilmelding] = useState<string>( EMPTY);
+  const [dokumentasjon, setDokumentasjon] = useState<File>();
+  const [dokumentasjonFeilmelding, setDokumentasjonFeilmelding] = useState<string>(EMPTY);
   const [bekreftet, setBekreftet] = useState<boolean>(props.bekreftet || false);
   const [bekreftetFeilmelding, setBekreftetFeilmelding] = useState<string>(props.bekreftetFeilmelding || EMPTY);
   const [videre, setVidere] = useState<boolean>(props.videre || false);
@@ -57,7 +50,7 @@ const GravidSide = (props: GravidSideProps) => {
     setDokumentasjon(file)
   }
 
-  const validateForm = () : boolean => {
+  const validateForm = (): boolean => {
     let feil = new Array<FeiloppsummeringFeil>();
 
     if (!fnrValid) {
@@ -84,7 +77,10 @@ const GravidSide = (props: GravidSideProps) => {
     if (tiltak === 'annet') {
       if (!tiltakBeskrivelse) {
         setTiltakBeskrivelseFeilmelding(REQUIRED_INPUT);
-        feil.push({ skjemaelementId: 'tilretteleggeFeilmeldingId', feilmelding: 'Spesifiser hvilke tiltak som er forsøkt' });
+        feil.push({
+          skjemaelementId: 'tilretteleggeFeilmeldingId',
+          feilmelding: 'Spesifiser hvilke tiltak som er forsøkt'
+        });
       } else {
         setTiltakBeskrivelseFeilmelding(EMPTY);
       }
@@ -134,19 +130,19 @@ const GravidSide = (props: GravidSideProps) => {
         <SoknadTittel>Søknad om utvidet støtte for gravid ansatts sykefravære</SoknadTittel>
 
         {status == GravidStatus.IN_PROGRESS &&
-          <GravidProgress />
+        <GravidProgress/>
         }
 
         {status == GravidStatus.SUCCESS &&
-          <GravidKvittering />
+        <GravidKvittering/>
         }
 
         {status == GravidStatus.ERROR &&
-          <GravidFeil />
+        <GravidFeil/>
         }
 
         {(status == GravidStatus.DEFAULT || status == GravidStatus.BAD_REQUEST) &&
-          <SideIndentering>
+        <SideIndentering>
 
           <Panel>
             <Ingress>
@@ -183,124 +179,135 @@ const GravidSide = (props: GravidSideProps) => {
               <RadioGruppe
                 legend="Har dere forsøkt å tilrettelegge arbeidsdagen slik at den ansatte kan utføre arbeidet sitt til
               tross for helsetilstanden hennes?">
-                <Radio label={'Ja'} name="sitteplass" value={'ja'} defaultChecked={tilrettelegge === true} onClick={() => {setTilrettelegge(true)}}/>
-                <Radio label={'Nei'} name="sitteplass" value={'nei'} defaultChecked={tilrettelegge === false} onClick={() => {setTilrettelegge(false)}}/>
+                <Radio label={'Ja'} name="sitteplass" value={'ja'} defaultChecked={tilrettelegge === true}
+                       onClick={() => {
+                         setTilrettelegge(true)
+                       }}/>
+                <Radio label={'Nei'} name="sitteplass" value={'nei'} defaultChecked={tilrettelegge === false}
+                       onClick={() => {
+                         setTilrettelegge(false)
+                       }}/>
               </RadioGruppe>
             </SkjemaGruppe>
 
             {tilrettelegge === true &&
-              <>
-                <SkjemaGruppe feilmeldingId='tilretteleggeFeilmeldingId'>
-                  <RadioGruppe
-                    feil={tiltakFeilmelding}
-                    legend="Hvilke tiltak er forsøkt/vurdert for at arbeidstaker skal kunne være i arbeid i svangerskapet?">
-                    <Radio label={'Fleksibel/tilpasset arbeidstid'} name="tiltak" onClick={() => {
-                      setTiltak('arbeidstid')
-                    }}
-                           defaultChecked={tiltak === 'arbeidstid'}/>
-                    <Radio label={'Hjemmekontor'} name="tiltak" onClick={() => {
-                      setTiltak('hjemmekontor')
-                    }}
-                           defaultChecked={tiltak === 'hjemmekontor'}/>
-                    <Radio label={'Tilpassede arbeidsoppgaver'} name="tiltak" onClick={() => {
-                      setTiltak('oppgaver')
-                    }}
-                           defaultChecked={tiltak === 'oppgaver'}/>
-                    <Radio label={'Annet, vennligst spesifiser kortfattet i feltet under'} name="tiltak" onClick={() => {
-                      setTiltak('annet')
-                    }}
-                           defaultChecked={tiltak === 'annet'}/>
-                    {tiltak === 'annet' &&
-                    <Textarea value={tiltakBeskrivelse} feil={tiltakBeskrivelseFeilmelding}
-                              onChange={(evt) => {
-                                setTiltakBeskrivelse(evt.currentTarget.value)
-                              }} />
-                    }
-                  </RadioGruppe>
-                </SkjemaGruppe>
+            <>
+              <SkjemaGruppe feilmeldingId='tilretteleggeFeilmeldingId'>
+                <RadioGruppe
+                  feil={tiltakFeilmelding}
+                  legend="Hvilke tiltak er forsøkt/vurdert for at arbeidstaker skal kunne være i arbeid i svangerskapet?">
+                  <Radio label={'Fleksibel/tilpasset arbeidstid'} name="tiltak" onClick={() => {
+                    setTiltak('arbeidstid')
+                  }}
+                         defaultChecked={tiltak === 'arbeidstid'}/>
+                  <Radio label={'Hjemmekontor'} name="tiltak" onClick={() => {
+                    setTiltak('hjemmekontor')
+                  }}
+                         defaultChecked={tiltak === 'hjemmekontor'}/>
+                  <Radio label={'Tilpassede arbeidsoppgaver'} name="tiltak" onClick={() => {
+                    setTiltak('oppgaver')
+                  }}
+                         defaultChecked={tiltak === 'oppgaver'}/>
+                  <Radio label={'Annet, vennligst spesifiser kortfattet i feltet under'} name="tiltak" onClick={() => {
+                    setTiltak('annet')
+                  }}
+                         defaultChecked={tiltak === 'annet'}/>
+                  {tiltak === 'annet' &&
+                  <Textarea value={tiltakBeskrivelse} feil={tiltakBeskrivelseFeilmelding}
+                            onChange={(evt) => {
+                              setTiltakBeskrivelse(evt.currentTarget.value)
+                            }}/>
+                  }
+                </RadioGruppe>
+              </SkjemaGruppe>
 
-                <SkjemaGruppe feil={omplasseringFeilmelding} feilmeldingId='omplasseringFeilmeldingId'>
-                  <RadioGruppe legend="Er omplassering av den ansatte forsøkt?">
-                    <Radio label={'Ja'} name="omplassering" defaultChecked={omplassering === 'ja'} onClick={() => {
-                      setOmplassering('ja')
-                    }}/>
-                    <Radio label={'Nei'} name="omplassering" defaultChecked={omplassering === 'nei'} onClick={() => {
-                      setOmplassering('nei')
-                    }}/>
-                    <Radio label={'Omplassering er ikke gjennomført'} name="omplassering"
-                           defaultChecked={omplassering === 'ikke'} onClick={() => {
-                      setOmplassering('ikke')
-                    }}/>
-                  </RadioGruppe>
-                </SkjemaGruppe>
-              </>
+              <SkjemaGruppe feil={omplasseringFeilmelding} feilmeldingId='omplasseringFeilmeldingId'>
+                <RadioGruppe legend="Er omplassering av den ansatte forsøkt?">
+                  <Radio label={'Ja'} name="omplassering" defaultChecked={omplassering === 'ja'} onClick={() => {
+                    setOmplassering('ja')
+                  }}/>
+                  <Radio label={'Nei'} name="omplassering" defaultChecked={omplassering === 'nei'} onClick={() => {
+                    setOmplassering('nei')
+                  }}/>
+                  <Radio label={'Omplassering er ikke gjennomført'} name="omplassering"
+                         defaultChecked={omplassering === 'ikke'} onClick={() => {
+                    setOmplassering('ikke')
+                  }}/>
+                </RadioGruppe>
+              </SkjemaGruppe>
+            </>
             }
 
             {tilrettelegge === false &&
-                <>
-                  <Skillelinje/>
-                  <Feilmelding>*Forsøksvis tilrettelegging er i utgangspunktet påkrevd for at vi skal godkjenne
-                    søknaden*</Feilmelding>
-                  <br />
-                  <Normaltekst>
-                    Dere kan
-                    <button onClick={() => {setVidere(true)}} className="lenke"
-                            style={{ display: 'run-in', border: 'none', margin: '0', paddingRight: '0' }}>gå videre med
-                      søknaden</button>,
-                    men det er altså da sannsynlig at den blir avslått.</Normaltekst>
-                </>
+            <>
+              <Skillelinje/>
+              <Feilmelding>*Forsøksvis tilrettelegging er i utgangspunktet påkrevd for at vi skal godkjenne
+                søknaden*</Feilmelding>
+              <br/>
+              <Normaltekst>
+                Dere kan
+                <button onClick={() => {
+                  setVidere(true)
+                }} className="lenke"
+                        style={{ display: 'run-in', border: 'none', margin: '0', paddingRight: '0' }}>gå videre med
+                  søknaden</button>,
+                men det er altså da sannsynlig at den blir avslått.</Normaltekst>
+            </>
             }
           </Panel>
 
           {(tilrettelegge || videre) &&
-            <>
-              <Skillelinje/>
+          <>
+            <Skillelinje/>
 
-              <Panel>
-                <SkjemaGruppe legend="Dokumentasjon om svagerskapsrelatert sykdomsfravære"
-                              feil={dokumentasjonFeilmelding}
-                              feilmeldingId='dokumentasjonFeilmeldingId'
-                              description="Det må dokumenteres av lege at fraværet er relatert til svangerskapsrelatert
+            <Panel>
+              <SkjemaGruppe legend="Dokumentasjon om svagerskapsrelatert sykdomsfravære"
+                            feil={dokumentasjonFeilmelding}
+                            feilmeldingId='dokumentasjonFeilmeldingId'
+                            description="Det må dokumenteres av lege at fraværet er relatert til svangerskapsrelatert
                         sykdom. Dere kan laste opp denne om dere har den. Alternativt vil NAV innhente dokumentasjon
                         direkte fra lege.">
-                  <Upload id="upload"
-                          label="Last opp dokumentasjon"
-                          extensions=".html,.pdf,.doc"
-                          onChange={handleUploadChanged}
-                          fileSize={250000}
-                          />
-                </SkjemaGruppe>
-              </Panel>
+                <Upload id="upload"
+                        label="Last opp dokumentasjon"
+                        extensions=".html,.pdf,.doc"
+                        onChange={handleUploadChanged}
+                        fileSize={250000}
+                />
+              </SkjemaGruppe>
+            </Panel>
 
-              <Skillelinje/>
+            <Skillelinje/>
 
-              <Panel>
-                <SkjemaGruppe feilmeldingId='bekreftFeilmeldingId'>
+            <Panel>
+              <SkjemaGruppe feilmeldingId='bekreftFeilmeldingId'>
                 <BekreftCheckboksPanel
                   label="Jeg bekrefter at opplysningene jeg har gitt er riktige."
                   checked={bekreftet}
                   feil={bekreftetFeilmelding}
-                  onChange={(evt) => {setBekreftet(!bekreftet)}}
+                  onChange={(evt) => {
+                    setBekreftet(!bekreftet)
+                  }}
                 >
-                  Jeg er kjent med at hvis opplysningene jeg har gitt ikke er riktige eller fullstendige, så kan jeg miste
+                  Jeg er kjent med at hvis opplysningene jeg har gitt ikke er riktige eller fullstendige, så kan jeg
+                  miste
                   retten til stønad.
                 </BekreftCheckboksPanel>
-                </SkjemaGruppe>
-              </Panel>
+              </SkjemaGruppe>
+            </Panel>
 
-              {feilOppsummeringer.length > 0 &&
-                <Panel>
-                  <Feiloppsummering
-                    tittel="For å gå videre må du rette opp følgende:"
-                    feil={feilOppsummeringer}
-                  />
-                </Panel>
-              }
+            {feilOppsummeringer.length > 0 &&
+            <Panel>
+              <Feiloppsummering
+                tittel="For å gå videre må du rette opp følgende:"
+                feil={feilOppsummeringer}
+              />
+            </Panel>
+            }
 
-              <Panel>
-                <Hovedknapp onClick={handleSubmitClicked}>Send søknad</Hovedknapp>
-              </Panel>
-            </>
+            <Panel>
+              <Hovedknapp onClick={handleSubmitClicked}>Send søknad</Hovedknapp>
+            </Panel>
+          </>
           }
 
         </SideIndentering>
