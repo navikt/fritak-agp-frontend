c0c979232f7d0393f6f0715bc8a6cd6d026232cd
Kent Fjellstad <kent.fjellstad@nav.no>
2020-11-03 15:13:53 +0100

    Renset bort ubrukt kode

--- src/components/gravid/GravideKvittering.test.tsx
+++ src/components/gravid/GravideKvittering.test.tsx
@@ -1,10 +1,8 @@
 import '@testing-library/jest-dom';
 import React from 'react'
-import { render, screen } from '@testing-library/react';
-import { axe, toHaveNoViolations } from 'jest-axe';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
 import GravidKvittering from "./GravidKvittering";
-
-expect.extend(toHaveNoViolations);
 
 describe('GravidKvittering', () => {
   it('should have no a11y violations', async () => {
