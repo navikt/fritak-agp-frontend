554c937f9c38127c3f48dacfad646da7188d8fd2
Kent Fjellstad <kent.fjellstad@nav.no>
2020-11-04 08:31:58 +0100

    Fix test

--- src/api/lagreGravidesoknad.test.ts
+++ src/api/lagreGravidesoknad.test.ts
@@ -1,69 +1,70 @@
-import lagreGravidesoknad, { RestStatus } from './lagreGravidesoknad';
+import lagreGravidesoknad from "./lagreGravidesoknad";
+import RestStatus from "./RestStatus";
 
-describe('lagreGravidesoknad', () => {
-  it('should resolve with status 200 if the backend responds with 200', async () => {
+describe("lagreGravidesoknad", () => {
+  it("should resolve with status 200 if the backend responds with 200", async () => {
     const mockData = {
-      mocked: 'OK'
-    }
+      mocked: "OK",
+    };
 
-    jest.spyOn(window, 'fetch').mockImplementationOnce(() =>
+    jest.spyOn(window, "fetch").mockImplementationOnce(() =>
       Promise.resolve({
         status: 200,
-        json: () => Promise.resolve(mockData)
+        json: () => Promise.resolve(mockData),
       } as Response)
     );
 
-    expect(await lagreGravidesoknad('/Path', {})).toEqual({ status: 200, responseData: mockData });
+    expect(await lagreGravidesoknad("/Path", {})).toEqual({ status: 200, validering: mockData });
   });
 
-  it('should reject with status Unauthorized if the backend responds with 401', async () => {
-    jest.spyOn(window, 'fetch').mockImplementationOnce(() =>
+  it("should reject with status Unauthorized if the backend responds with 401", async () => {
+    jest.spyOn(window, "fetch").mockImplementationOnce(() =>
       Promise.resolve({
         status: 401,
-        json: () => Promise.resolve({})
+        json: () => Promise.resolve({}),
       } as Response)
     );
 
-    expect(await lagreGravidesoknad('/Path', {})).toEqual({ status: RestStatus.Unauthorized, responseData: [] });
+    expect(await lagreGravidesoknad("/Path", {})).toEqual({ status: RestStatus.Unauthorized, validering: [] });
   });
 
-  it('should reject with status Error if the backend responds with 500', async () => {
-    jest.spyOn(window, 'fetch').mockImplementationOnce(() =>
+  it("should reject with status Error if the backend responds with 500", async () => {
+    jest.spyOn(window, "fetch").mockImplementationOnce(() =>
       Promise.resolve({
         status: 500,
-        json: () => Promise.resolve({})
+        json: () => Promise.resolve({}),
       } as Response)
     );
 
-    expect(await lagreGravidesoknad('/Path', {})).toEqual({ status: RestStatus.Error, responseData: [] });
+    expect(await lagreGravidesoknad("/Path", {})).toEqual({ status: RestStatus.Error, validering: [] });
   });
 
-  it('should reject with status Unknown if the backend responds with an unknown response', async () => {
-    jest.spyOn(window, 'fetch').mockImplementationOnce(() =>
+  it("should reject with status Unknown if the backend responds with an unknown response", async () => {
+    jest.spyOn(window, "fetch").mockImplementationOnce(() =>
       Promise.resolve({
         status: 1234,
-        json: () => Promise.resolve({})
-      } as Response));
-
-    expect(await lagreGravidesoknad('/Path', {})).toEqual({ status: RestStatus.Unknown, responseData: [] });
-  });
-
-  it('should reject with status Timeout if the backend does not respond', async () => {
-    jest.useFakeTimers();
-
-    jest.spyOn(window, 'fetch').mockImplementationOnce(() =>
-    Promise.resolve({
-      status: -33,
-      json: () => Promise.reject({
-      }),
-    } as Response)
+        json: () => Promise.resolve({}),
+      } as Response)
     );
 
-    const resultat = lagreGravidesoknad('/Path', {});
+    expect(await lagreGravidesoknad("/Path", {})).toEqual({ status: RestStatus.Unknown, validering: [] });
+  });
+
+  it("should reject with status Timeout if the backend does not respond", async () => {
+    jest.useFakeTimers();
+
+    jest.spyOn(window, "fetch").mockImplementationOnce(() =>
+      Promise.resolve({
+        status: -33,
+        json: () => Promise.reject({}),
+      } as Response)
+    );
+
+    const resultat = lagreGravidesoknad("/Path", {});
 
     jest.advanceTimersByTime(15000);
 
-    expect( await resultat).toEqual({status: RestStatus.Timeout, responseData: []});
+    expect(await resultat).toEqual({ status: RestStatus.Timeout, validering: [] });
 
     jest.useRealTimers();
   });
