c8fa06400f68ac3b9689ee7abd6a9e3df6fa9e5e
Morten Laukvik <morten.laukvik@nav.no>
2020-11-04 15:32:11 +0100

    Bruk helsesjekker

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ArbeidsgiverProvider, EnvironmentProvider, InnloggetSide } from '@navikt/helse-arbeidsgiver-felles-frontend';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './locales/i18n';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GravidSide from './components/gravid/GravidSide';
 import KroniskSide from './components/KroniskSide';
 import Forside from './components/Forside';
@@ -14,17 +14,19 @@ const App = () => {
   return (
     <EnvironmentProvider loginServiceUrl={env.loginServiceUrl} sideTittel={'Søknadsskjema'}
                          basePath={env.baseUrl}>
-      <ArbeidsgiverProvider>
-        <I18nextProvider i18n={i18n}>
-          <InnloggetSide className="innloggetside__fritak">
-            <Switch>
-              <Route path="/gravid" render={() => <GravidSide/>}/>
-              <Route path="/kronisk" render={() => <KroniskSide/>}/>
-              <Route path="/" render={() => <Forside/>}/>
-            </Switch>
-          </InnloggetSide>
-        </I18nextProvider>
-      </ArbeidsgiverProvider>
+      <BrowserRouter basename="fritak-agp">
+        <ArbeidsgiverProvider>
+          <I18nextProvider i18n={i18n}>
+            <InnloggetSide className="innloggetside__fritak">
+              <Switch>
+                <Route path="/gravid" render={() => <GravidSide/>}/>
+                <Route path="/kronisk" render={() => <KroniskSide/>}/>
+                <Route path="/" render={() => <Forside/>}/>
+              </Switch>
+            </InnloggetSide>
+          </I18nextProvider>
+        </ArbeidsgiverProvider>
+      </BrowserRouter>
     </EnvironmentProvider>
   );
 };
