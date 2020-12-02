416aa697797ae377669e08bb2e648af6f890909f
Kent Fjellstad <kent.fjellstad@nav.no>
2020-12-02 13:10:32 +0100

    Mere tester

--- src/api/handleStatus.test.ts
+++ src/api/handleStatus.test.ts
@@ -16,6 +16,56 @@ describe('handleStatus', () => {
     expect(await handleStatus(params)).toEqual({ iam: 'happy' });
   });
 
+  it('Should return a text when json object fails', async () => {
+    const mockData = {
+      iam: 'happy'
+    };
+
+    const params = ({
+      status: 200,
+      json: () => Promise.reject(mockData),
+      text: () => Promise.resolve('FAIL'),
+      clone: () => ({
+        json: () => Promise.reject(mockData),
+        text: () => Promise.resolve('FAIL')
+      })
+    } as unknown) as Response;
+
+    expect(await handleStatus(params)).toEqual('FAIL');
+  });
+
+  it('Should return a valid json object on 400 response', async () => {
+    const mockData = {
+      iam: 'happy'
+    };
+
+    const params = ({
+      status: 400,
+      json: () => Promise.resolve(mockData),
+      clone: () => ({ json: () => Promise.reject(mockData) })
+    } as unknown) as Response;
+
+    expect(handleStatus(params)).rejects.toEqual({ iam: 'happy' });
+  });
+
+  it('Should return a text when json object fails for 400 response', async () => {
+    const mockData = {
+      iam: 'happy'
+    };
+
+    const params = ({
+      status: 400,
+      json: () => Promise.reject(mockData),
+      text: () => Promise.resolve('FAIL'),
+      clone: () => ({
+        json: () => Promise.reject(mockData),
+        text: () => Promise.resolve('FAIL')
+      })
+    } as unknown) as Response;
+
+    expect(handleStatus(params)).rejects.toBe('FAIL');
+  });
+
   it('Should reject a promisse with code 401 for response with status 401', () => {
     const params = ({
       status: 401,
