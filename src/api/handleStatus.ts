cc33c78501798c6762bffa2baa08100fdf52912f
Kent Fjellstad <kent.fjellstad@nav.no>
2020-12-08 09:31:00 +0100

    Fall trough

--- src/api/handleStatus.ts
+++ src/api/handleStatus.ts
@@ -3,21 +3,15 @@ import RestStatus from './RestStatus';
 const handleStatus = (response: Response) => {
   switch (response.status) {
     case 200:
+    case 400:
       return response
         .clone()
         .json()
         .catch(() => response.text());
     case RestStatus.Unauthorized:
       return Promise.reject(RestStatus.Unauthorized);
-    case 400:
-      return response
-        .clone()
-        .json()
-        .catch(() => response.text());
-
     case 500:
       return response.json();
-    // return Promise.reject(RestStatus.Error);
     default:
       return Promise.reject(RestStatus.Unknown);
   }
