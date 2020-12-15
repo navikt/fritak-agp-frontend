166d5366a132a90ada57f6e7d4670b4f113273bb
Kent Fjellstad <kent.fjellstad@nav.no>
2020-12-15 14:23:50 +0100

    Test av Upload komponenten

---
+++ src/components/Upload.test.tsx
@@ -0,0 +1,99 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import Upload from './Upload';
+
+describe('Upload', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('should render nicely', () => {
+    render(
+      <Upload
+        id='1'
+        label='Upload'
+        extensions='jpg'
+        fileSize={123}
+        onChange={jest.fn()}
+      />
+    );
+    expect(screen.getByText('Upload')).toBeInTheDocument();
+  });
+
+  it('should change the button label when a file is selected', () => {
+    const mockChangeFn = jest.fn();
+    render(
+      <Upload
+        id='1'
+        label='Upload'
+        extensions='jpg'
+        fileSize={123}
+        onChange={mockChangeFn}
+      />
+    );
+
+    const uploadInput = screen.getByLabelText('Upload');
+    fireEvent.change(uploadInput, {
+      target: {
+        files: [
+          {
+            size: 10,
+            name: 'filename'
+          }
+        ]
+      }
+    });
+    expect(screen.getByText('filename')).toBeInTheDocument();
+    expect(mockChangeFn).toHaveBeenLastCalledWith({
+      size: 10,
+      name: 'filename'
+    });
+  });
+
+  it('should show an error when the file is to big, text on button should stay the same', () => {
+    const mockChangeFn = jest.fn();
+    render(
+      <Upload
+        id='1'
+        label='UploadFile'
+        extensions='gif'
+        fileSize={150}
+        onChange={mockChangeFn}
+      />
+    );
+
+    const uploadInput = screen.getByLabelText('UploadFile');
+    fireEvent.change(uploadInput, {
+      target: {
+        files: [
+          {
+            size: 200,
+            name: 'AnotherFilename'
+          }
+        ]
+      }
+    });
+    expect(screen.getByText('UploadFile')).toBeInTheDocument();
+    expect(screen.getByText('Filen er for stor')).toBeInTheDocument();
+    expect(mockChangeFn).toHaveBeenLastCalledWith({
+      size: 200,
+      name: 'AnotherFilename'
+    });
+  });
+
+  it('should have no a11y violations', async () => {
+    const { container } = render(
+      <Upload
+        id='1'
+        label='Upload'
+        extensions='jpg'
+        fileSize={123}
+        onChange={jest.fn()}
+      />
+    );
+
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+});
