5598f2d9c3b03380ada65305b98b79b9551e2be7
Morten Laukvik <morten.laukvik@nav.no>
2021-01-08 14:43:16 +0100

    Ikke bruk i18n

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {
   LoginProvider
 } from '@navikt/helse-arbeidsgiver-felles-frontend';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './locales/i18n';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GravidSide from './components/gravid/GravidSide';
 import Forside from './components/Forside';
@@ -22,21 +21,19 @@ const App = () => {
       basePath={env.baseUrl}
     >
       <BrowserRouter basename='fritak-agp'>
-        <I18nextProvider i18n={i18n}>
-          <LoginProvider loginServiceUrl={env.loginServiceUrl}>
-            <Side>
-              <Switch>
-                <Route path={lenker.Gravid} render={() => <GravidSide />} />
-                <Route path={lenker.Kronisk} render={() => <KroniskSide />} />
-                <Route
-                  path={lenker.GravidKvittering}
-                  render={() => <GravidKvittering />}
-                />
-                <Route path={lenker.Home} render={() => <Forside />} />
-              </Switch>
-            </Side>
-          </LoginProvider>
-        </I18nextProvider>
+        <LoginProvider loginServiceUrl={env.loginServiceUrl}>
+          <Side>
+            <Switch>
+              <Route path={lenker.Gravid} render={() => <GravidSide />} />
+              <Route path={lenker.Kronisk} render={() => <KroniskSide />} />
+              <Route
+                path={lenker.GravidKvittering}
+                render={() => <GravidKvittering />}
+              />
+              <Route path={lenker.Home} render={() => <Forside />} />
+            </Switch>
+          </Side>
+        </LoginProvider>
       </BrowserRouter>
     </EnvironmentProvider>
   );
