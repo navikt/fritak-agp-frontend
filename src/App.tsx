ac86f108bf39fe97171f5b3a06043d74933e59ce
Kent Fjellstad <kent.fjellstad@nav.no>
2021-01-18 14:24:13 +0100

    Ubrukt

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
         '/fritak-agp'
       );
       window.location.href = redirectedLoginServiceUrl;
-      return <div className='login-context-redirect' />;
+      return <div />;
     }
   });
 
