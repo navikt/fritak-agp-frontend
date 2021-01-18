088a2e26833bc03118e71f0bff541fae1749d8c6
Jeppe Feld <jeppe.feld@nav.no>
2021-01-18 11:08:22 +0100

    Fix duplicate id

--- src/components/kronisk/KroniskSide.tsx
+++ src/components/kronisk/KroniskSide.tsx
@@ -252,7 +252,7 @@ const KroniskSide = () => {
 
                     <Textarea
                       label='Annet'
-                      id='annet'
+                      id='textarea-annet'
                       value={state.kommentar || ''}
                       feil={state.kommentarError || undefined}
                       onChange={(evt) =>
