9c69280a460a37d4219aa0567b444e7712add0aa
Morten Laukvik <morten.laukvik@nav.no>
2021-01-22 10:33:29 +0100

    Refaktorering og opprydding

--- src/components/kronisk/mapValidationResponse.ts
+++ src/components/kronisk/mapValidationResponse.ts
@@ -3,10 +3,48 @@ import ValidationResponse from '../../api/ValidationResponse';
 import { FeiloppsummeringFeil } from 'nav-frontend-skjema';
 import { lagFeil } from '../lagFeil';
 
-export const mapValidationResponse = (
-  response: ValidationResponse,
-  state: KroniskState
-): KroniskState => {
+export const mapFeilmeldinger = (response: ValidationResponse, state: KroniskState) => {
+  const feilmeldinger = new Array<FeiloppsummeringFeil>();
+  response.violations.forEach((v) => {
+    switch (v.propertyPath) {
+      case 'fnr':
+        state.fnrError = v.message;
+        feilmeldinger.push(lagFeil('fnr', v.message));
+        break;
+      case 'orgnr':
+        state.orgnrError = v.message;
+        feilmeldinger.push(lagFeil('orgnr', v.message));
+        break;
+      case 'kommentar':
+        state.kommentarError = v.message;
+        feilmeldinger.push(lagFeil('kommentar', v.message));
+        break;
+      case 'arbeidstyper':
+        state.arbeidError = v.message;
+        feilmeldinger.push(lagFeil('arbeidsutfører', v.message));
+        break;
+      case 'paakjenningstyper':
+        state.paakjenningerError = v.message;
+        feilmeldinger.push(lagFeil('paakjenninger', v.message));
+        break;
+      case 'bekreftet':
+        state.bekreftError = v.message;
+        feilmeldinger.push(lagFeil('bekreft', v.message));
+        break;
+      case 'dokumentasjon':
+        state.dokumentasjonError = v.message;
+        feilmeldinger.push(lagFeil('dokumentasjon', v.message));
+        break;
+      case 'fravaer':
+        state.fravaerError = v.message;
+        feilmeldinger.push(lagFeil('fravaer', v.message));
+        break;
+    }
+  });
+  return feilmeldinger;
+};
+
+export const mapValidationResponse = (response: ValidationResponse, state: KroniskState): KroniskState => {
   const nextState = Object.assign({}, state);
   switch (response.status) {
     case 201:
@@ -22,48 +60,10 @@ export const mapValidationResponse = (
       nextState.accessDenied = true;
       return nextState;
     case 422:
-      const feilmeldinger = new Array<FeiloppsummeringFeil>();
-      response.violations.forEach((v) => {
-        switch (v.propertyPath) {
-          case 'fnr':
-            nextState.fnrError = v.message;
-            feilmeldinger.push(lagFeil('fnr', v.message));
-            break;
-          case 'orgnr':
-            nextState.orgnrError = v.message;
-            feilmeldinger.push(lagFeil('orgnr', v.message));
-            break;
-          case 'kommentar':
-            nextState.kommentarError = v.message;
-            feilmeldinger.push(lagFeil('kommentar', v.message));
-            break;
-          case 'arbeidstyper':
-            nextState.arbeidError = v.message;
-            feilmeldinger.push(lagFeil('arbeidsutfører', v.message));
-            break;
-          case 'paakjenningstyper':
-            nextState.paakjenningerError = v.message;
-            feilmeldinger.push(lagFeil('paakjenninger', v.message));
-            break;
-          case 'bekreftet':
-            nextState.bekreftError = v.message;
-            feilmeldinger.push(lagFeil('bekreft', v.message));
-            break;
-          case 'dokumentasjon':
-            nextState.dokumentasjonError = v.message;
-            feilmeldinger.push(lagFeil('dokumentasjon', v.message));
-            break;
-          case 'fravaer':
-            nextState.fravaerError = v.message;
-            feilmeldinger.push(lagFeil('fravaer', v.message));
-            break;
-        }
-      });
-
       nextState.kvittering = false;
       nextState.progress = false;
       nextState.error = false;
-      nextState.feilmeldinger = feilmeldinger;
+      nextState.feilmeldinger = mapFeilmeldinger(response, nextState);
       nextState.accessDenied = false;
       return nextState;
     default:
@@ -71,9 +71,7 @@ export const mapValidationResponse = (
       nextState.kvittering = false;
       nextState.progress = false;
       nextState.feilmeldinger = new Array<FeiloppsummeringFeil>();
-      nextState.feilmeldinger.push(
-        lagFeil('ukjent', 'Klarte ikke å sende inn skjema. Prøv igjen senere.')
-      );
+      nextState.feilmeldinger.push(lagFeil('ukjent', 'Klarte ikke å sende inn skjema. Prøv igjen senere.'));
       return nextState;
   }
 };
