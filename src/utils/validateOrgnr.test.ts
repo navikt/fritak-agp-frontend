ca9b54f86c31367a9e1de0c59262462e01d29ba4
Morten Laukvik <morten.laukvik@nav.no>
2021-01-28 10:33:03 +0100

    POH-609

--- src/utils/validateOrgnr.test.ts
+++ src/utils/validateOrgnr.test.ts
@@ -11,14 +11,12 @@ describe('validateOrgnr', () => {
   it('should give error when empty value and required', async () => {
     expect(validateOrgnr('', true)).toEqual('Mangler organisasjonsnummer');
   });
-
   it('should give error when invalid value and required', async () => {
     expect(validateOrgnr('123', true)).toEqual('Ugyldig organisasjonsnummer');
   });
   it('should not give error when invalid value and not required', async () => {
     expect(validateOrgnr('123', false)).toBeUndefined();
   });
-
   it('should not give error when not required and undefined value', async () => {
     expect(validateOrgnr(undefined, false)).toBeUndefined();
     expect(validateOrgnr()).toBeUndefined();
