ab35ffaf520c34070e0c8997ec60bff2288cbe5d
Morten Laukvik <morten.laukvik@nav.no>
2021-01-28 10:28:21 +0100

    POH-609

--- src/components/gravidkrav/validateFra.test.ts
+++ src/components/gravidkrav/validateFra.test.ts
@@ -2,13 +2,15 @@ import { validateFra } from './validateFra';
 import { parseDato } from '../../utils/Dato';
 
 describe('validateFra', () => {
+  it('should not show error when valid value and not required', () => {
+    expect(validateFra(parseDato('01.02.2020'), true)).toBeUndefined();
+  });
+
   it('should not show error when empty value and not required', () => {
-    // expect(validateFra(undefined, false)).toBeUndefined();
     expect(validateFra(parseDato(''), false)).toBeUndefined();
   });
 
   it('should show error when empty value and required', () => {
-    // expect(validateFra(undefined, true)).not.toBeUndefined();
     expect(validateFra(parseDato(''), true)).not.toBeUndefined();
   });
 
@@ -20,7 +22,7 @@ describe('validateFra', () => {
     expect(validateFra(parseDato('05.10.2020'), false)).toBeUndefined();
   });
 
-  // it('should show error when illegal dato', () => {
-  //   expect(validateFra(parseDato('32.10.2020'), false)).not.toBeUndefined();
-  // });
+  it('should show error when illegal dato', () => {
+    expect(validateFra(parseDato('99.99.2020'), true)).not.toBeUndefined();
+  });
 });
--- src/utils/validateOrgnr.test.ts
+++ src/utils/validateOrgnr.test.ts
@@ -1,8 +1,9 @@
 import { validateOrgnr } from './validateOrgnr';
+import testOrgnr from '../mockData/testOrgnr';
 
 describe('validateOrgnr', () => {
   it('should not give error when valid fnr', async () => {
-    expect(validateOrgnr('917404437', true)).toBeUndefined();
+    expect(validateOrgnr(testOrgnr.GyldigeOrgnr.TestOrg1, true)).toBeUndefined();
   });
   it('should give error when undefined value and required', async () => {
     expect(validateOrgnr(undefined, true)).toEqual('Mangler organisasjonsnummer');
