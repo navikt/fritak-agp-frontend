7d8c096f05ee7cef14eba286c0c1072a702eb2be
Kent Fjellstad <kent.fjellstad@nav.no>
2021-02-22 10:14:07 +0100

    Kommentarer som ikke Sonar liker

--- src/api/mapResponse.ts
+++ src/api/mapResponse.ts
@@ -1,38 +1,3 @@
-// import ValidationResponse from './ValidationResponse';
-// import HttpStatus from './HttpStatus';
-// import map201 from '../validation/map201';
-// import map400 from '../validation/map400';
-// import map401 from '../validation/map401';
-// import map422 from '../validation/map422';
-// import map500 from '../validation/map500';
-// import { ValidationState } from '../validation/ValidationState';
-// import { FeiloppsummeringFeil } from 'nav-frontend-skjema';
-
-// const mapResponse = (
-//   response: ValidationResponse,
-//   state: ValidationState,
-//   mapFeilmeldinger: (response: ValidationResponse, state: ValidationState) => FeiloppsummeringFeil[]
-// ): ValidationState => {
-//   const nextState = Object.assign({}, state);
-//   switch (response.status) {
-//     case HttpStatus.Created:
-//       return map201(nextState);
-//     case HttpStatus.BadRequest:
-//       return map400(nextState);
-//     case HttpStatus.Unauthorized:
-//       return map401(nextState);
-//     case HttpStatus.UnprocessableEntity:
-//       nextState.feilmeldinger = mapFeilmeldinger(response, nextState);
-//       return map422(nextState);
-//     case HttpStatus.Error:
-//       return map500(nextState);
-//     default:
-//       return nextState;
-//   }
-// };
-
-// export default mapResponse;
-
 import ValidationResponse from './ValidationResponse';
 import HttpStatus from './HttpStatus';
 import map201 from '../validation/map201';
