d2571631a0e387b97e75e96ef21733c68a096526
Kent Fjellstad <kent.fjellstad@nav.no>
2021-02-23 12:28:20 +0100

    til validator på felles sted

---
+++ src/validation/validateTil.test.ts
@@ -0,0 +1,31 @@
+import { validateTil } from './validateTil';
+import { parseDato } from '../utils/Dato';
+
+describe('validateTil', () => {
+  it('should not show errors when valid', () => {
+    expect(validateTil(parseDato('01.02.2020'), parseDato('03.04.2020'), false)).toBeUndefined();
+    expect(validateTil(parseDato('01.02.2020'), parseDato('03.04.2020'), true)).toBeUndefined();
+  });
+
+  it('should show errors when errors in dates', () => {
+    expect(validateTil(parseDato('aa.02.2020'), parseDato('03.04.2020'), true)).not.toBeUndefined();
+    expect(validateTil(parseDato('01.02.2020'), parseDato('aa.04.2020'), true)).not.toBeUndefined();
+  });
+
+  it('should show errors when tom is before fom', () => {
+    expect(validateTil(parseDato('03.04.2020'), parseDato('01.02.2020'), true)).not.toBeUndefined();
+  });
+
+  it('should not show errors when valid and equal dates', () => {
+    expect(validateTil(parseDato('01.02.2020'), parseDato('01.02.2020'), false)).toBeUndefined();
+    expect(validateTil(parseDato('01.02.2020'), parseDato('01.02.2020'), true)).toBeUndefined();
+  });
+
+  it('should not show errors when fra is undefined', () => {
+    expect(validateTil(undefined, parseDato('03.04.2020'), true)).toBeUndefined();
+  });
+
+  it('should not show errors when til is undefined', () => {
+    expect(validateTil(parseDato('03.04.2020'), undefined, true)).toBeUndefined();
+  });
+});
