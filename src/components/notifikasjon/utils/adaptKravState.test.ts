7df3782da50015aa0cdecb8bfbe2f2b48e8d5453
Kent Fjellstad <kent.fjellstad@nav.no>
2021-03-09 15:25:55 +0100

    Splitt og test

---
+++ src/components/notifikasjon/utils/adaptKravState.test.ts
@@ -0,0 +1,161 @@
+import GravidKravResponse from '../../../api/gravidkrav/GravidKravResponse';
+import KroniskKravResponse from '../../../api/gravidkrav/KroniskKravResponse';
+import adaptKravState from './adaptKravState';
+
+describe('adaptKravState', () => {
+  it('shold convert the thing correctly', () => {
+    const input: KroniskKravResponse = {
+      id: 'f68a073b-9bed-45c1-aa1a-4dad2de52c2e',
+      opprettet: '2021-03-02T12:50:50.400688',
+      sendtAv: '10107400090',
+      virksomhetsnummer: '810007842',
+      identitetsnummer: '22018520056',
+      perioder: [
+        {
+          fom: '2021-03-01',
+          tom: '2021-03-10',
+          antallDagerMedRefusjon: 2,
+          beloep: 1234.0
+        }
+      ],
+      harVedlegg: false,
+      kontrollDager: null,
+      journalpostId: '493346012',
+      oppgaveId: null,
+      virksomhetsnavn: 'Ukjent arbeidsgiver'
+    };
+
+    const expected: GravidKravResponse = {
+      harVedlegg: false,
+      id: 'f68a073b-9bed-45c1-aa1a-4dad2de52c2e',
+      identitetsnummer: '22018520056',
+      journalpostId: '493346012',
+      kontrollDager: null,
+      oppgaveId: null,
+      opprettet: '2021-03-02T12:50:50.400688',
+      periode: {
+        antallDagerMedRefusjon: 2,
+        beloep: 1234,
+        fom: '2021-03-01',
+        tom: '2021-03-10'
+      },
+      sendtAv: '10107400090',
+      virksomhetsnavn: 'Ukjent arbeidsgiver',
+      virksomhetsnummer: '810007842'
+    };
+
+    expect(adaptKravState(input)).toEqual(expected);
+  });
+
+  it('shold convert the thing with two perioder correctly', () => {
+    const input: KroniskKravResponse = {
+      id: 'f68a073b-9bed-45c1-aa1a-4dad2de52c2e',
+      opprettet: '2021-03-02T12:50:50.400688',
+      sendtAv: '10107400090',
+      virksomhetsnummer: '810007842',
+      identitetsnummer: '22018520056',
+      perioder: [
+        {
+          fom: '2021-03-01',
+          tom: '2021-03-10',
+          antallDagerMedRefusjon: 2,
+          beloep: 1234.0
+        },
+        {
+          fom: '2021-04-01',
+          tom: '2021-04-10',
+          antallDagerMedRefusjon: 3,
+          beloep: 1234.0
+        }
+      ],
+      harVedlegg: false,
+      kontrollDager: null,
+      journalpostId: '493346012',
+      oppgaveId: null,
+      virksomhetsnavn: 'Ukjent arbeidsgiver'
+    };
+
+    const expected: GravidKravResponse = {
+      harVedlegg: false,
+      id: 'f68a073b-9bed-45c1-aa1a-4dad2de52c2e',
+      identitetsnummer: '22018520056',
+      journalpostId: '493346012',
+      kontrollDager: null,
+      oppgaveId: null,
+      opprettet: '2021-03-02T12:50:50.400688',
+      periode: {
+        antallDagerMedRefusjon: 5,
+        beloep: 2468,
+        fom: '2021-03-01',
+        tom: '2021-04-10'
+      },
+      sendtAv: '10107400090',
+      virksomhetsnavn: 'Ukjent arbeidsgiver',
+      virksomhetsnummer: '810007842'
+    };
+
+    expect(adaptKravState(input)).toEqual(expected);
+  });
+
+  it('shold convert the thing with four perioder correctly', () => {
+    const input: KroniskKravResponse = {
+      id: 'f68a073b-9bed-45c1-aa1a-4dad2de52c2e',
+      opprettet: '2021-03-02T12:50:50.400688',
+      sendtAv: '10107400090',
+      virksomhetsnummer: '810007842',
+      identitetsnummer: '22018520056',
+      perioder: [
+        {
+          fom: '2021-03-01',
+          tom: '2021-03-10',
+          antallDagerMedRefusjon: 2,
+          beloep: 1234.0
+        },
+        {
+          fom: '2021-02-01',
+          tom: '2021-02-10',
+          antallDagerMedRefusjon: 2,
+          beloep: 1234.0
+        },
+        {
+          fom: '2021-05-01',
+          tom: '2021-05-10',
+          antallDagerMedRefusjon: 4,
+          beloep: 1234.0
+        },
+        {
+          fom: '2021-04-01',
+          tom: '2021-04-10',
+          antallDagerMedRefusjon: 1,
+          beloep: 1234.0
+        }
+      ],
+      harVedlegg: false,
+      kontrollDager: null,
+      journalpostId: '493346012',
+      oppgaveId: null,
+      virksomhetsnavn: 'Ukjent arbeidsgiver'
+    };
+
+    const expected: GravidKravResponse = {
+      harVedlegg: false,
+      id: 'f68a073b-9bed-45c1-aa1a-4dad2de52c2e',
+      identitetsnummer: '22018520056',
+      journalpostId: '493346012',
+      kontrollDager: null,
+      oppgaveId: null,
+      opprettet: '2021-03-02T12:50:50.400688',
+      periode: {
+        antallDagerMedRefusjon: 9,
+        beloep: 4936,
+        fom: '2021-02-01',
+        tom: '2021-05-10'
+      },
+      sendtAv: '10107400090',
+      virksomhetsnavn: 'Ukjent arbeidsgiver',
+      virksomhetsnummer: '810007842'
+    };
+
+    expect(adaptKravState(input)).toEqual(expected);
+  });
+});
---
+++ src/components/notifikasjon/utils/adaptKravState.ts
@@ -0,0 +1,25 @@
+import dayjs from 'dayjs';
+import GravidKravResponse from '../../../api/gravidkrav/GravidKravResponse';
+import KroniskKravResponse from '../../../api/gravidkrav/KroniskKravResponse';
+
+const adaptKravState = (krav: KroniskKravResponse): GravidKravResponse => ({
+  id: krav.id,
+  opprettet: krav.opprettet,
+  sendtAv: krav.sendtAv,
+  virksomhetsnummer: krav.virksomhetsnummer,
+  identitetsnummer: krav.identitetsnummer,
+  periode: krav.perioder.reduce((prev, current) => {
+    prev.beloep += current.beloep;
+    prev.antallDagerMedRefusjon += current.antallDagerMedRefusjon;
+    prev.fom = dayjs(prev.fom).isBefore(dayjs(current.fom)) ? prev.fom : current.fom;
+    prev.tom = dayjs(prev.tom).isAfter(dayjs(current.tom)) ? prev.tom : current.tom;
+    return prev;
+  }),
+  harVedlegg: krav.harVedlegg,
+  kontrollDager: krav.kontrollDager,
+  journalpostId: krav.journalpostId,
+  oppgaveId: krav.oppgaveId,
+  virksomhetsnavn: krav.virksomhetsnavn
+});
+
+export default adaptKravState;
