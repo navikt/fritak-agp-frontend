a3eef18d94294cf63907b69952aa9614a23f52eb
Morten Laukvik <morten.laukvik@nav.no>
2021-06-11 10:20:43 +0200

    Bruk LanguageProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { ApplicationRoutes } from './ApplicationRoutes';
 import { Organisasjon } from '@navikt/bedriftsmeny/lib/organisasjon';
 import env from './config/environment';
