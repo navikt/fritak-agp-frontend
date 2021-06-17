40d7f6058bd6357229868146a5eae86b98c9c009
Morten Laukvik <morten.laukvik@nav.no>
2021-06-17 11:26:26 +0200

    Nye språknøkler

--- src/validation/validateDager.ts
+++ src/validation/validateDager.ts
@@ -1,13 +1,19 @@
 import ValidationResult from '@navikt/helse-arbeidsgiver-felles-frontend/dist/validation/ValidationResult';
 
-export interface ValidateDagerResult extends ValidationResult {
-  key: 'VALIDATE_DAGER_MISSING' | 'VALIDATE_DAGER_REQUIRED';
+export enum ValidateDagerKeys {
+  VALIDATE_DAGER_MISSING = 'VALIDATE_DAGER_MISSING',
+  VALIDATE_DAGER_REQUIRED = 'VALIDATE_DAGER_REQUIRED'
 }
+
+export interface ValidateDagerResult extends ValidationResult {
+  key: ValidateDagerKeys.VALIDATE_DAGER_MISSING | ValidateDagerKeys.VALIDATE_DAGER_REQUIRED;
+}
+
 export const validateDager = (dager: number | undefined, required: boolean): ValidateDagerResult | undefined => {
   if (!dager) {
-    return required ? { key: 'VALIDATE_DAGER_MISSING' } : undefined;
+    return required ? { key: ValidateDagerKeys.VALIDATE_DAGER_MISSING } : undefined;
   }
-  return dager === undefined ? { key: 'VALIDATE_DAGER_REQUIRED' } : undefined;
+  return dager === undefined ? { key: ValidateDagerKeys.VALIDATE_DAGER_REQUIRED } : undefined;
 };
 
 export default validateDager;
