d3d862f0e529a5b9f280d99f9af88e099baaeef4
Kent Fjellstad <kent.fjellstad@nav.no>
2021-09-03 12:51:05 +0200

    Tekster som erstatter backend koder

---
+++ src/components/notifikasjon/gravid/soknad/gravidSoknadOmplassering.ts
@@ -0,0 +1,8 @@
+const gravidSoknadOmplassering = {
+  MOTSETTER: 'den ansatte ønsker ikke omplassering',
+  FAAR_IKKE_KONTAKT: 'vi får ikke kontakt med den ansatte.',
+  IKKE_ANDRE_OPPGAVER: 'vi har ikke andre oppgaver eller arbeidssteder å tilby.',
+  HELSETILSTANDEN: 'den ansatte vil ikke fungere i en annen jobb på grunn av helsetilstanden.'
+};
+
+export default gravidSoknadOmplassering;
