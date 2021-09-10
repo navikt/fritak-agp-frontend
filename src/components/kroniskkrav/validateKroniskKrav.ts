67c10ed7a0aab3b92c4adb40de5c7ef5163b321e
Kent Fjellstad <kent.fjellstad@nav.no>
2021-09-10 09:52:00 +0200

    Opprydding

--- src/components/kroniskkrav/validateKroniskKrav.ts
+++ src/components/kroniskkrav/validateKroniskKrav.ts
@@ -14,7 +14,6 @@ import { i18n } from 'i18next';
 import validateArbeidsdager from '../../validation/validateArbeidsdager';
 import { MAX_ARBEIDSDAGER, MIN_ARBEIDSDAGER } from '../../config/konstanter';
 import formatValidation from '../../utils/formatValidation';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
 import dayjs from 'dayjs';
 import Locales from '../../locale/Locales';
 
@@ -22,7 +21,6 @@ const MAX = 10000000;
 const MIN_DATE = new Date(2021, 1, 1);
 
 export const validateKroniskKrav = (state: KroniskKravState, translate: i18n): KroniskKravState => {
-  dayjs.extend(customParseFormat);
   if (!state.validated) {
     return state;
   }
@@ -40,15 +38,12 @@ export const validateKroniskKrav = (state: KroniskKravState, translate: i18n): K
   nextState.perioder?.forEach((aktuellPeriode) => {
     const minDato = dayjs(MIN_DATE).format('DD.MM.YYYY');
     const valideringFraStatus = validateFra(aktuellPeriode.fom, MIN_DATE, !!state.validated);
-    let transString = Locales[valideringFraStatus?.key as any].nb;
 
-    aktuellPeriode.fomError = translate.t(transString, { value: minDato });
+    aktuellPeriode.fomError = translate.t(valideringFraStatus?.key as any, { value: minDato });
 
     const valideringTilStatus = validateTil(aktuellPeriode.fom, aktuellPeriode.tom, MIN_DATE, !!state.validated);
-    debugger;
-    transString = Locales[valideringTilStatus?.key as any].nb;
 
-    aktuellPeriode.tomError = translate.t(transString, { value: minDato });
+    aktuellPeriode.tomError = translate.t(valideringTilStatus?.key as any, { value: minDato });
 
     aktuellPeriode.dagerError = formatValidation(validateDager(aktuellPeriode.dager, !!state.validated), translate);
     aktuellPeriode.belopError = formatValidation(
