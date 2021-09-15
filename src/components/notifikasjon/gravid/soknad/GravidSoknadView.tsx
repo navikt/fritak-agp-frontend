e041fbcd01ce9413709636e6958794a793d9faa3
Kent Fjellstad <kent.fjellstad@nav.no>
2021-09-15 11:56:36 +0200

    Formatering av gravid søknad notifikasjon

--- src/components/notifikasjon/gravid/soknad/GravidSoknadView.tsx
+++ src/components/notifikasjon/gravid/soknad/GravidSoknadView.tsx
@@ -39,8 +39,10 @@ const GravidSoknadView = ({ gravidSoknadResponse }: GravidSoknadNotifikasjonProp
 
       <p>{formatTilrettelegge(gravidSoknadResponse.tilrettelegge)}</p>
 
-      <p>Tiltak forsøkt for at den ansatte skal kunne jobbe:</p>
-      {formatTiltak(gravidSoknadResponse.tiltak, gravidSoknadResponse.tiltakBeskrivelse)}
+      <p>
+        Tiltak forsøkt for at den ansatte skal kunne jobbe:
+        {formatTiltak(gravidSoknadResponse.tiltak, gravidSoknadResponse.tiltakBeskrivelse)}
+      </p>
 
       <p>{formatOmplassering(gravidSoknadResponse.omplassering, gravidSoknadResponse.omplasseringAarsak)}</p>
 
--- src/components/notifikasjon/gravid/soknad/formatTiltak.tsx
+++ src/components/notifikasjon/gravid/soknad/formatTiltak.tsx
@@ -2,7 +2,7 @@ import formatTiltakBeskrivelse from './formatTiltakBeskrivelse';
 import React from 'react';
 
 const formatTiltak = (tiltak: string[], tiltakBeskrivelse: string) => (
-  <ul>
+  <ul className='dash'>
     {tiltak.map((t) => (
       <li key={t}>{formatTiltakBeskrivelse(t, tiltakBeskrivelse)}</li>
     ))}
