ce9fc9a578e328a0b0cfbe694dccd29fa8d078fd
Kent Fjellstad <kent.fjellstad@nav.no>
2021-09-22 14:22:10 +0200

    Poh 1053 begrensing antall perioder (#211)
    * Mindre enn min eller mer enn max
    
    * Tester for å verifisere

--- src/validation/validateAntallPerioder.ts
+++ src/validation/validateAntallPerioder.ts
@@ -17,9 +17,9 @@ export const validateAntallPerioder = (
   maxDager: number = 366
 ): ValidateAntallPerioderResult | undefined => {
   if (!required) return undefined;
-  if (!dager) return { key: ValidateAntallPerioderKeys.VALIDATE_ANTALL_PERIODER_MISSING };
-  if (dager <= minDager) return { key: ValidateAntallPerioderKeys.VALIDATE_ANTALL_PERIODER_TOO_LOW };
-  if (maxDager <= dager) return { key: ValidateAntallPerioderKeys.VALIDATE_ANTALL_PERIODER_TOO_HIGH };
+  if (dager === undefined) return { key: ValidateAntallPerioderKeys.VALIDATE_ANTALL_PERIODER_MISSING };
+  if (dager < minDager) return { key: ValidateAntallPerioderKeys.VALIDATE_ANTALL_PERIODER_TOO_LOW };
+  if (maxDager < dager) return { key: ValidateAntallPerioderKeys.VALIDATE_ANTALL_PERIODER_TOO_HIGH };
   return undefined;
 };
 
