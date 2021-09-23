55d7921ce6ddd02d5b770beedb500b0cb447f09f
Kent Fjellstad <kent.fjellstad@nav.no>
2021-09-23 15:18:17 +0200

    Tester

---
+++ src/components/notifikasjon/kronisk/soknad/Paakjenninger.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Paakjenninger from './Paakjenninger';
+
+describe('Paakjenninger', () => {
+  it('should print out the list of stuff', () => {
+    const paakjenninger = [
+      'ALLERGENER',
+      'UKOMFORTABEL',
+      'STRESSENDE',
+      'REGELMESSIG',
+      'GAAING',
+      'HARDE',
+      'TUNGE',
+      'ANNET'
+    ];
+
+    render(<Paakjenninger paakjenninger={paakjenninger} beskrivelse='Cheese' />);
+
+    expect(screen.getByText(/Allergener eller giftstoffer/)).toBeInTheDocument();
+    expect(screen.getByText(/Ukomfortabel temperatur eller luftfuktighet/)).toBeInTheDocument();
+    expect(screen.getByText(/Stressende omgivelser/)).toBeInTheDocument();
+    expect(screen.getByText(/Regelmessige kveldsskift eller nattskift/)).toBeInTheDocument();
+    expect(screen.getByText('Mye gåing/ståing')).toBeInTheDocument();
+    expect(screen.getByText(/Harde gulv/)).toBeInTheDocument();
+    expect(screen.getByText(/Tunge løft/)).toBeInTheDocument();
+    expect(screen.getByText(/Annet: /)).toBeInTheDocument();
+    expect(screen.getByText(/"Cheese"/)).toBeInTheDocument();
+  });
+
+  it('should not print anything', () => {
+    const { container } = render(<Paakjenninger />);
+
+    expect(container.firstChild).toBeNull();
+  });
+});
