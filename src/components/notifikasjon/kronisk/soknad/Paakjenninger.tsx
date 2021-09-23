a5376578d66ff6dd77ed18c8ad6d1542d305217c
Kent Fjellstad <kent.fjellstad@nav.no>
2021-09-23 11:31:39 +0200

    Manglende filer

---
+++ src/components/notifikasjon/kronisk/soknad/Paakjenninger.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import tekstPaakjenninger from './tekstPaakjenninger';
+
+interface PaakjenningerProps {
+  paakjenninger?: string[];
+  beskrivelse?: string;
+}
+
+const Paakjenninger = (props: PaakjenningerProps) => {
+  if (!props.paakjenninger) {
+    return null;
+  }
+
+  return (
+    <ul className='dash'>
+      {props.paakjenninger.map((paakjenning) => (
+        <li key={paakjenning}>
+          {tekstPaakjenninger[paakjenning] || paakjenning.toLowerCase()}
+          {paakjenning === 'ANNET' ? ' "' + props.beskrivelse + '"' : ''}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Paakjenninger;
