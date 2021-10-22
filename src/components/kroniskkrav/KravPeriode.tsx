1d7a83153e01416aa5cfd6e56dae3efcf5b8dbd4
Kent Fjellstad <kent.fjellstad@nav.no>
2021-10-22 17:54:32 +0200

    Begrenser antall desimaler

--- src/components/kroniskkrav/KravPeriode.tsx
+++ src/components/kroniskkrav/KravPeriode.tsx
@@ -13,7 +13,7 @@ import './KravPeriode.scss';
 import { useTranslation } from 'react-i18next';
 import LangKey from '../../locale/LangKey';
 import { Fareknapp } from 'nav-frontend-knapper';
-import { beregnSykemeldingGradering } from '../../api/kroniskkrav/mapPeriodeData';
+import beregnRefusjon from './beregnRefusjon';
 
 interface KravPeriodeProps {
   dispatch: any;
@@ -22,24 +22,6 @@ interface KravPeriodeProps {
   lonnspliktDager: number | undefined;
   slettbar: boolean;
 }
-
-const beregnRefusjon = (enkeltPeriode: KroniskKravPeriode, lonnspliktDager: number | undefined): number => {
-  if (!enkeltPeriode.belop || !enkeltPeriode.dager || !enkeltPeriode.grunnbeloep || !lonnspliktDager) {
-    return 0;
-  }
-
-  const aarsBelop = enkeltPeriode.belop * 12;
-  const aarsGrunnbelop = enkeltPeriode.grunnbeloep * 6;
-  const grad = beregnSykemeldingGradering(enkeltPeriode.sykemeldingsgrad);
-
-  if (aarsBelop > aarsGrunnbelop) {
-    const gRefusjon = (aarsGrunnbelop / lonnspliktDager) * enkeltPeriode.dager;
-    return (Math.round((gRefusjon + Number.EPSILON) * 100) / 100) * grad;
-  } else {
-    const aarsRefusjon = (aarsBelop / lonnspliktDager) * enkeltPeriode.dager;
-    return (Math.round((aarsRefusjon + Number.EPSILON) * 100) / 100) * grad;
-  }
-};
 
 const KravPeriode = (props: KravPeriodeProps) => {
   const { t } = useTranslation();
