6d4f18bd8273fdc90ed2947c9a60a9c39212cc19
Kent Fjellstad <kent.fjellstad@nav.no>
2021-10-25 11:01:37 +0200

    Fjernet lenke og endret tekst

--- src/components/felles/Orgnr/Orgnr.tsx
+++ src/components/felles/Orgnr/Orgnr.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Input } from 'nav-frontend-skjema';
 import Hjelpetekst from 'nav-frontend-hjelpetekst';
-import Lenke from 'nav-frontend-lenker';
 import { useTranslation } from 'react-i18next';
 import LangKey from '../../../locale/LangKey';
+import { Normaltekst } from 'nav-frontend-typografi';
 
 interface OrgnrProps {
   label: string;
@@ -25,11 +25,7 @@ const Orgnr = (props: OrgnrProps) => {
         <div style={{ display: 'flex' }}>
           {props.label}
           <Hjelpetekst style={{ marginLeft: '0.5rem' }}>
-            {t(LangKey.ORGNR_HJELPETEKST)}
-            <br />
-            <Lenke href='https://www.altinn.no/starte-og-drive/starte/registrering/organisasjonsnummer-virksomhetsnummer/'>
-              {t(LangKey.ORGNR_LENKE)}
-            </Lenke>
+            <Normaltekst>{t(LangKey.ORGNR_HJELPETEKST)}</Normaltekst>
           </Hjelpetekst>
         </div>
       }
