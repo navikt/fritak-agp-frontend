a7f39a499483031ff6625b94d74cbfbb3d0bb220
Kent Fjellstad <kent.fjellstad@nav.no>
2021-11-25 14:57:05 +0100

    Oppdatert felles til å bruke peerDependencies

--- tests/gravid-krav.js
+++ tests/gravid-krav.js
@@ -236,7 +236,7 @@ test('Legg til og fjern perioder', async (t) => {
 
   await t
     .scrollBy(0, 200)
-    .click(ReactSelector('Fareknapp').withText('Slett'))
+    .click(ReactSelector('Fareknapp').withText('SLETT'))
     .expect(Selector('#belop-0').visible)
     .ok()
     .expect(Selector('#belop-1').visible)
