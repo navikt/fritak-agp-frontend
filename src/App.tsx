1525d4d78c7d6b7b443b769a971a5f59afe08309
Kent Fjellstad <kent.fjellstad@nav.no>
2022-03-09 12:00:18 +0100

    Kjører lokalt

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { ApplicationRoutes } from './ApplicationRoutes';
 import { Organisasjon } from '@navikt/bedriftsmeny/lib/organisasjon';
-import env from './config/environment';
+import environment, { EnvironmentType } from './config/environment';
 import {
   LanguageProvider,
   ArbeidsgiverStatus,
@@ -12,6 +12,7 @@ import {
 } from '@navikt/helse-arbeidsgiver-felles-frontend';
 import Locales from './locale/Locales';
 import i18next from 'i18next';
+import getCookie from './api/CookiePlease/getCookie';
 
 interface ApplicationProps {
   loginStatus?: LoginStatus;
@@ -25,8 +26,8 @@ export const Application = ({
   loginStatus = LoginStatus.Checking,
   arbeidsgiverStatus = ArbeidsgiverStatus.NotStarted,
   arbeidsgivere,
-  basePath = env.baseUrl,
-  loginServiceUrl = env.loginServiceUrl
+  basePath = environment.baseUrl,
+  loginServiceUrl = environment.loginServiceUrl
 }: ApplicationProps) => (
   <Route path='/:language(nb|en)/*'>
     <LoginProvider baseUrl={basePath} status={loginStatus} loginServiceUrl={loginServiceUrl}>
@@ -38,6 +39,11 @@ export const Application = ({
 );
 
 const App = () => {
+  useEffect(() => {
+    if (environment.environmentMode === EnvironmentType.LOCAL) {
+      getCookie();
+    }
+  });
   return (
     <BrowserRouter basename='fritak-agp'>
       <LanguageProvider languages={['nb', 'en']} i18n={i18next} bundle={Locales}>
