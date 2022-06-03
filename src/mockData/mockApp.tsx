0f7e7488bc1b66115feba53bc6ba13380b8f29e8
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-06-03 10:33:57 +0200

    Fjerner loginStatus

--- src/mockData/mockApp.tsx
+++ src/mockData/mockApp.tsx
@@ -1,14 +1,10 @@
 import { Router } from 'react-router-dom';
-import { ArbeidsgiverProvider, ArbeidsgiverStatus, LoginStatus } from '@navikt/helse-arbeidsgiver-felles-frontend';
+import { ArbeidsgiverProvider, ArbeidsgiverStatus } from '@navikt/helse-arbeidsgiver-felles-frontend';
 import React from 'react';
 import mockHistory from './mockHistory';
 import { ApplicationRoutes } from '../ApplicationRoutes';
 
-export const mockApp = (
-  children: any = ApplicationRoutes(),
-  path: string = '/',
-  loginStatus: LoginStatus = LoginStatus.Verified
-) => (
+export const mockApp = (children: any = ApplicationRoutes(), path: string = '/') => (
   <Router history={mockHistory(path)}>
     <ArbeidsgiverProvider arbeidsgivere={[]} status={ArbeidsgiverStatus.Successfully} baseUrl={''}>
       {children}
