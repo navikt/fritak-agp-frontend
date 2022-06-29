a7f37e02399dc55108d4bb6da24c6c76bfdc101f
Kent Fjellstad <kent.fjellstad@nav.no>
2022-06-29 07:59:25 +0200

    Får alive testen opp og kjører igjen

--- testalive/isalive.ts
+++ testalive/isalive.ts
@@ -1,10 +1,7 @@
 import { Selector } from 'testcafe';
-import { waitForReact } from 'testcafe-react-selectors';
 
-fixture`Enkeltinnsending`.page('https://arbeidsgiver.nav.no/fritak-agp/nb/kronisk/soknad').beforeEach(async () => {
-  await waitForReact();
-});
+fixture`Enkeltinnsending`.page('https://arbeidsgiver.nav.no/fritak-agp/nb/kronisk/soknad');
 
 test('Bli videresendt til minid loginsiden', async (t) => {
-  await t.expect(Selector('html').textContent).contains('BankID');
+  await t.expect(Selector('html').textContent).contains('VELFERDSETATEN');
 });
