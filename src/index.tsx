abb0d12e7007f2c15e826c6b3e41ee90cc35d418
Kent Fjellstad <kent.fjellstad@nav.no>
2023-04-24 12:49:07 +0200

    Oppdateringer (#422)
    * Oppgraderer knappekomponenten
    
    * Tester som måtte justeres
    
    * Ymse oppgraderinger og testfikser

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,7 @@ import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
 import 'nav-frontend-core/dist/main.css';
 import 'nav-frontend-typografi-style/dist/main.css';
 import 'nav-frontend-skjema-style/dist/main.css';
-import 'nav-frontend-knapper-style/dist/main.css';
 import 'nav-frontend-hjelpetekst-style/dist/main.css';
-import 'nav-frontend-alertstriper-style/dist/main.css';
 import 'nav-frontend-grid-style/dist/main.css';
 import 'nav-frontend-chevron-style/dist/main.css';
 import 'nav-frontend-lenker-style/dist/main.css';
@@ -22,11 +20,13 @@ import 'nav-frontend-veilederpanel-style/dist/main.css';
 import 'nav-frontend-modal-style/dist/main.css';
 import 'nav-frontend-popover-style/dist/main.css';
 import 'nav-frontend-tabell-style/dist/main.css';
+import 'nav-frontend-knapper-style/dist/main.css';
 import 'nav-frontend-ekspanderbartpanel-style/dist/main.css';
 import '@navikt/helse-arbeidsgiver-felles-frontend/dist/library.css';
 import env, { EnvironmentType } from './config/environment';
 import './components/felles/Upload/Upload.sass';
 import { Integrations } from '@sentry/tracing';
+import '@navikt/ds-css';
 
 if (env.environmentMode !== EnvironmentType.LOCAL) {
   Sentry.init({
