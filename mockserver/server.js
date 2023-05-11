5beed91cf25875ae6863b80b758f313ffad91362
Kent Fjellstad <kent.fjellstad@nav.no>
2023-05-11 12:34:37 +0200

    Limit på mockserver bruken

--- mockserver/server.js
+++ mockserver/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
+const rateLimit = require('express-rate-limit');
+
 const app = express();
 const cors = require('cors');
 
 const options = {
   root: path.join(__dirname)
 };
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
 
 app.use(cors());
 
