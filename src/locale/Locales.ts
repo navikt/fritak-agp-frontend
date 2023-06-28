18761bf73a0a081652ecb2220267ef66672c169e
Kent Fjellstad <kent.fjellstad@nav.no>
2023-06-28 15:44:07 +0200

    Revert "Oppdatering av kronisk fravær teksten"
    This reverts commit b0000feedab65039d974b726c04ea19658279313.

--- src/locale/Locales.ts
+++ src/locale/Locales.ts
@@ -607,8 +607,8 @@ const Locales: Record<
   KRONISK_SIDE_FRAVAER_DESCRIPTION: {
     nb:
       'For å vurdere retten til fritak fra arbeidsgiverperiode ser vi på arbeidstakerens historiske fravær. Dere kan gå ' +
-      '2 år tilbake i tid hvis både arbeidsforholdet og helseproblemene har vart så lenge. Angi antall dager med sykefravær ' +
-      'i hver måned. Oppgi alle egenmeldte dager, samt hele og graderte sykmeldingsdager.',
+      '2 år tilbake i tid hvis både arbeidsforholdet og helseproblemene har vart så lenge. Angi antall dager med ' +
+      'sykefravær i hver måned.',
     en:
       "To assess the right to exemption from the employer period, we look at the employee's historical absence. You can " +
       'go 2 years back in time if both the employment relationship and the health problems have lasted that long. Enter ' +
