1d066e5ef84ff92c410f9d099aa3cc9066a69402
Kent Fjellstad <kent.fjellstad@nav.no>
2023-11-03 10:35:59 +0100

    Oppdatering av github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: '',
+  base: '/fritak-agp/',
   plugins: [react(), viteTsconfigPaths()],
   server: {
     // this ensures that the browser opens upon server start
