10e41bb9250ce4fcafcb54842e41101492e926d5
Kent Fjellstad <kent.fjellstad@nav.no>
2024-09-05 07:41:31 +0200

    Oppdaterte tekster (#877)

--- src/components/notifikasjon/felles/NotifikasjonUkjent.tsx
+++ src/components/notifikasjon/felles/NotifikasjonUkjent.tsx
@@ -3,6 +3,6 @@ import { Alert } from '@navikt/ds-react';
 
 export const NotifikasjonUkjent = () => (
   <Alert variant='info' className='notifikasjon__ukjent'>
-    Fant ikke
+    Kravet er slettet av arbeidsgiver
   </Alert>
 );
