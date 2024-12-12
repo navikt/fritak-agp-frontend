9458e94eabb16d6c9e8f1f403cfeaa1d4dca0318
Kent Fjellstad <kent.fjellstad@nav.no>
2024-12-12 10:47:17 +0100

    Oppdatering av sass og tester

--- src/components/felles/Feilmeldingspanel/Feilmeldingspanel.test.tsx
+++ src/components/felles/Feilmeldingspanel/Feilmeldingspanel.test.tsx
@@ -2,8 +2,25 @@ import React from 'react';
 import Feilmeldingspanel from './Feilmeldingspanel';
 import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
+import { useTranslation } from 'react-i18next';
+import { vi } from 'vitest';
+
+vi.mock('react-i18next', () => ({
+  useTranslation: vi.fn()
+}));
 
 describe('Feilmeldingspanel', () => {
+  beforeEach(() => {
+    const useTranslationSpy = useTranslation;
+    const tSpy = vi.fn((str) => str);
+    useTranslationSpy.mockReturnValue({
+      t: tSpy,
+      i18n: {
+        changeLanguage: () => new Promise(() => {})
+      }
+    });
+  });
+
   it('should have no a11y violations', async () => {
     const rendered = render(<Feilmeldingspanel feilmeldinger={[]} />);
     const results = await axe(rendered.container);
