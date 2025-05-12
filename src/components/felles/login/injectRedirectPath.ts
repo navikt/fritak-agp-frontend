e027437d2c9c6757fd631eee8e47f1ecae0316ca
Kent Fjellstad <kent.fjellstad@nav.no>
2025-05-12 11:48:47 +0200

    Mer ubrukt kode

--- src/components/felles/login/injectRedirectPath.ts
+++
@@ -1,9 +0,0 @@
-import Language from '../../../locale/Language';
-
-function injectRedirectPath(loginServiceUrl: string, injectedPath: string, lang: Language): string {
-  const host = window.location.origin;
-  const injectedUrl = loginServiceUrl.replace('XXX', host + injectedPath);
-  return injectedUrl.replace(':language', lang);
-}
-
-export default injectRedirectPath;
