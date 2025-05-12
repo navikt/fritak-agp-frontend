0f912b7e233310178c1b1c28bed02f76c6c1b647
Kent Fjellstad <kent.fjellstad@nav.no>
2025-05-12 10:56:23 +0200

    Oppdatert tekster

--- src/components/felles/Feilmeldingspanel/FeilmeldingspanelLanguage.tsx
+++
@@ -1,9 +0,0 @@
-import { FeilmeldingspanelKeys } from './FeilmeldingspanelKeys';
-import { Locale } from '../../../locale/Locales';
-
-export const FeilmeldingspanelLanguage: Record<FeilmeldingspanelKeys, Locale> = {
-  FEILMELDINGSPANEL: {
-    nb: 'For å gå videre må du rette opp følgende:',
-    en: 'To proceed, you must correct the following:'
-  }
-};
--- src/locale/Locales.ts
+++ src/locale/Locales.ts
@@ -763,11 +763,9 @@ const Locales: Record<
   INGEN_TILGANG_ADVARSEL: {
     nb:
       'Du har ikke rettigheter til å søke om refusjon for noen bedrifter\n' +
-      'Tildeling av roller foregår i Altinn\n' +
       '[Les mer om roller og tilganger](https://arbeidsgiver.nav.no/min-side-arbeidsgiver/informasjon-om-tilgangsstyring)',
     en:
       'You do not have the rights to apply for reimbursement for any companies\n' +
-      'Assignment of roles takes place in Altinn\n' +
       '[Read more about roles and accesses](https://arbeidsgiver.nav.no/min-side-arbeidsgiver/informasjon-om-tilgangsstyring)'
   },
   LOGGET_UT_ADVARSEL_LOGGET_UT: {
