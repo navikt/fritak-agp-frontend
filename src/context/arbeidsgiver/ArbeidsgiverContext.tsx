2602e9f2908e5ccbce8a29c7f053c9d70a901532
Kent Fjellstad <kent.fjellstad@nav.no>
2025-06-11 08:18:46 +0200

    Fjerner ArbeidsgiverStatus til fordel for HttpStatus, som egentlig er det samme

--- src/context/arbeidsgiver/ArbeidsgiverContext.tsx
+++ src/context/arbeidsgiver/ArbeidsgiverContext.tsx
@@ -1,9 +1,9 @@
 import React, { createContext, useContext, useEffect, useState, useMemo, PropsWithChildren } from 'react';
 import { Organisasjon } from '@navikt/virksomhetsvelger';
 import ArbeidsgiverAPI from '../../api/arbeidsgiver/ArbeidsgiverAPI';
-import ArbeidsgiverStatus from './ArbeidsgiverStatus';
 import ArbeidsgiverInterface from './ArbeidsgiverInterface';
 import { Loader } from '@navikt/ds-react';
+import HttpStatus from '../../api/HttpStatus';
 
 const buildArbeidsgiverContext = (firma: string, arbeidsgiverId: string, arbeidsgivere: Organisasjon[]) =>
   ({
@@ -15,7 +15,7 @@ const buildArbeidsgiverContext = (firma: string, arbeidsgiverId: string, arbeids
 const ArbeidsgiverContext = createContext(buildArbeidsgiverContext('', '', []));
 
 interface ArbeidsgiverContextProviderProps {
-  status?: ArbeidsgiverStatus;
+  status?: HttpStatus;
   arbeidsgivere?: Organisasjon[];
   firma?: string;
   arbeidsgiverId?: string;
@@ -25,14 +25,14 @@ interface ArbeidsgiverContextProviderProps {
 const useArbeidsgiver = () => useContext(ArbeidsgiverContext);
 
 const ArbeidsgiverProvider = (props: PropsWithChildren<ArbeidsgiverContextProviderProps>) => {
-  const [loadingStatus, setLoadingStatus] = useState<number>(props.status || ArbeidsgiverStatus.NotStarted);
+  const [loadingStatus, setLoadingStatus] = useState<number>(props.status || HttpStatus.NotStarted);
   const [arbeidsgivere, setArbeidsgivere] = useState<Organisasjon[]>(props.arbeidsgivere || []);
   const [firma, setFirma] = useState<string>(props.firma || '');
   const [arbeidsgiverId, setArbeidsgiverId] = useState<string>(props.arbeidsgiverId || '');
 
   useEffect(() => {
-    if (loadingStatus == ArbeidsgiverStatus.NotStarted) {
-      setLoadingStatus(ArbeidsgiverStatus.Started);
+    if (loadingStatus == HttpStatus.NotStarted) {
+      setLoadingStatus(HttpStatus.Started);
       ArbeidsgiverAPI.GetArbeidsgivere(props.baseUrl).then((res) => {
         setLoadingStatus(res.status);
         setArbeidsgivere(res.organisasjoner);
@@ -52,7 +52,7 @@ const ArbeidsgiverProvider = (props: PropsWithChildren<ArbeidsgiverContextProvid
     [arbeidsgivere, setArbeidsgivere, firma, setFirma, arbeidsgiverId, setArbeidsgiverId]
   );
 
-  if (loadingStatus === ArbeidsgiverStatus.NotStarted || loadingStatus === ArbeidsgiverStatus.Started) {
+  if (loadingStatus === HttpStatus.NotStarted || loadingStatus === HttpStatus.Started) {
     return <Loader size='3xlarge' title='Laster data' className='arbeidsgiver-context-spinner' />;
   }
 
